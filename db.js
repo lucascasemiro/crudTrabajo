66ccc96b01c9b617c00a941f05d7ecfa7cef1517
Lucas Casemiro <141570809+lucascasemiro@users.noreply.github.com>
2025-06-03 08:25:38 -0300

    hdgfy

--- db.js
+++ db.js
@@ -24,28 +24,48 @@ async function connect() {
 
 connect();
 
-async function selectCostumers() {
+async function selectCostumer(id){
+  const client = await connect();
+  const res = await client.query("SELECT * FROM client WHERE cpf=$1", [id]);
+  return res.rows;
+}
+async function selectCostumers(costumer){
   const client = await connect();
   const res = await client.query("SELECT * FROM client");
   return res.rows;
-  }
-
-  async function selectCostumer(id) {
-    const client = await connect();
-    const res = await client.query("SELECT * FROM client WHERE cpf=$1", [id]);
-    return res.rows;
-    }
+}
 
 async function insertCostumers(customer) {
+
   const client = await connect()
+
   const sql = "INSERT INTO client(cpf, nome, email, idade, profissao) VALUES($1, $2, $3, $4, $5)"
+
   const values = [ customer.cpf, customer.nome, customer.email, customer.idade, customer.profissao ];
+
   await client.query(sql, values);
 
 }  
+async function deleteCostumer(id) {
+  const client = await connect();
+  const sql = "DELETE FROM client WHERE cpf=$1";
+  const values = [id];
+  await client.query(sql, values)
+  }
 
 module.exports = {
   insertCostumers,
   selectCostumers,
-  selectCostumer
-  }
+  selectCostumer,
+  deleteCostumer
+}
+
+app.get('/client', async (req, res) => {
+  const clientes = await db.selectCostumers();
+  res.json(clientes);
+});
+
+app.delete("/client/:id", async (req, res) => {
+  await db.deleteCustomer(req.params.id)
+  res.sendStatus(204)
+  })
